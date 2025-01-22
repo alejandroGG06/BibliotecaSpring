7352a8fcedd33abe93f8b402b0fdae1425fd659a
Alejandro <grajalesalejandro068@gmail.com>
2025-01-22 19:55:19 +0100

    Prestamo inicios

---
+++ src/main/java/org/menus/biblioteca/Prestamocontro.java
@@ -0,0 +1,12 @@
+package org.menus.biblioteca;
+
+public class Prestamocontro {
+
+    PrestamoRepo prestamoRepo;
+    public Prestamocontro() {}
+
+    public Prestamocontro(PrestamoRepo prestamoRepo) {
+        this.prestamoRepo = prestamoRepo;
+    }
+
+}
