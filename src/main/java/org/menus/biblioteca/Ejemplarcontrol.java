53647f0c8674d06dab0d864c08c98c8797a9a038
Alejandro <grajalesalejandro068@gmail.com>
2025-01-22 21:21:56 +0100

    Final

--- src/main/java/org/menus/biblioteca/Ejemplarcontrol.java
+++ src/main/java/org/menus/biblioteca/Ejemplarcontrol.java
@@ -2,10 +2,9 @@ package org.menus.biblioteca;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +24,20 @@ public Ejemplarcontrol(EjemplarRepo ejemplarRepo) {
     System.out.println(ejemplars);
     return ResponseEntity.ok(ejemplars);
 }
+@PostMapping(value = "/addejem",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Ejemplar>addprestamo( @RequestBody Ejemplar ejemplar  ) {
+        this.ejemplarRepo.save(ejemplar);
+    return ResponseEntity.ok().body(ejemplar);
+}
+@PutMapping("/{id}")
+public ResponseEntity<Ejemplar>addprestamo(@PathVariable int id ,@RequestBody Ejemplar ejemplar  ) {
+ Ejemplar ejemplar1 = this.ejemplarRepo.save(ejemplar);
+    return ResponseEntity.ok().body(ejemplar1);
+}
+@DeleteMapping("/{id}")
+    public ResponseEntity<String>deleteprestamo(@PathVariable int id) {
+    this.ejemplarRepo.deleteById(id);
+    String mensaje="Ejemplar eliminado";
+    return ResponseEntity.ok().body(mensaje);}
 
 }
